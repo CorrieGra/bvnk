a7ed8a9770678b62e1c6f338af1605996c48013f
Corrie Graham <corriegdev@gmail.com>
2025-04-07 18:00:22 +0200

    chore(data): Core data fetching & handling

--- src/pages/quote/accept.tsx
+++ src/pages/quote/accept.tsx
@@ -1,6 +1,89 @@
+import axios, { AxiosResponse } from 'axios';
 import { Typography } from 'components/atoms';
+import { Select } from 'components/molecule';
+import { Quote } from 'dto/quote';
+import { useCallback, useEffect, useState } from 'react';
+import { useNavigate, useParams } from 'react-router';
+
+type AcceptPageParamas = {
+	UUID: string;
+};
+
+const formatTimeFromTimestamps = (expiryDate: number) => {
+	const msLeft = expiryDate - Date.now();
+
+	if (msLeft <= 0) return '00:00:00';
+
+	const totalSeconds = Math.floor(msLeft / 1000);
+	const hours = Math.floor(totalSeconds / 3600);
+	const minutes = Math.floor((totalSeconds % 3600) / 60);
+	const seconds = totalSeconds % 60;
+
+	const pad = (n: number) => n.toString().padStart(2, '0');
+
+	return `${pad(hours)}:${pad(minutes)}:${pad(seconds)}`;
+};
 
 export const AcceptQuotePage = () => {
+	const navigate = useNavigate();
+	const { UUID } = useParams<AcceptPageParamas>();
+	const [quote, setQuote] = useState<Quote>();
+	const [payinCurrency, setPayinCurrency] = useState<string>('');
+
+	useEffect(() => {
+		const getQuote = async () => {
+			const quoteResponse = await axios.get<Quote>(
+				`https://api.sandbox.bvnk.com/api/v1/pay/${UUID}/summary`,
+			);
+
+			setQuote(quoteResponse.data);
+		};
+
+		void getQuote();
+	}, []);
+
+	useEffect(() => {
+		if (!!payinCurrency) {
+			const data = {
+				currency: payinCurrency,
+				payInMethod: 'crypto',
+			};
+
+			const updateQuote = async () => {
+				try {
+					const quoteResponse = await axios.put<Quote>(
+						`https://api.sandbox.bvnk.com/api/v1/pay/${UUID}/update/summary`,
+						data,
+					);
+
+					const { data: updatedQuote } = quoteResponse;
+					setQuote({ ...quote, ...updatedQuote });
+				} catch (error) {
+					navigate(`/payin/${UUID}/expired`);
+				}
+			};
+
+			void updateQuote();
+		}
+	}, [payinCurrency]);
+
+	const handleCurrencySelect = useCallback((currency: string) => {
+		setPayinCurrency(currency);
+	}, []);
+
+	const handleConfirmation = useCallback(async () => {
+		try {
+			const data = { successUrl: 'no_url' };
+			await axios.put(
+				`https://api.sandbox.bvnk.com/api/v1/pay/${UUID}/accept/summary`,
+				data,
+			);
+			navigate(`/payin/${UUID}/pay`);
+		} catch (error) {
+			navigate(`/payin/${UUID}/expired`);
+		}
+	}, []);
+
 	return (
 		<>
 			<div
@@ -12,12 +95,12 @@ export const AcceptQuotePage = () => {
 				}}
 			>
 				<Typography
-					value='Merchant Name'
+					value={quote?.merchantDisplayName}
 					variant='title'
 					colour='gray'
 				/>
 				<Typography
-					value='200 USD'
+					value={`${quote?.displayCurrency.amount} ${quote?.displayCurrency.currency}`}
 					variant='heading'
 					colour='gray'
 				/>
@@ -29,7 +112,7 @@ export const AcceptQuotePage = () => {
 						padding={{ right: 0.25 }}
 					/>
 					<Typography
-						value='REF123457890'
+						value={`${quote?.reference}`}
 						variant='caption'
 						colour='gray'
 						padding={{ left: 0.25 }}
@@ -37,56 +120,77 @@ export const AcceptQuotePage = () => {
 				</span>
 			</div>
 			<div style={{ display: 'flex', flexDirection: 'column' }}>
-				{/* TO BE REPLACED WITH INPUT MOLECULE */}
-				<select>
-					<option value='1'>Option 1</option>
-					<option value='2'>Option 2</option>
-					<option value='3'>Option 3</option>
-				</select>
+				<Select
+					label='Pay with'
+					placeholder='Select a currency'
+					value={payinCurrency}
+					onChange={handleCurrencySelect}
+					options={[
+						{
+							label: 'Bitcoin',
+							value: 'BTC',
+						},
+						{
+							label: 'Ethereum',
+							value: 'ETH',
+						},
+						{
+							label: 'Litecoin',
+							value: 'LTC',
+						},
+					]}
+				/>
 
-				<div
-					style={{
-						display: 'flex',
-						flexDirection: 'row',
-						alignItems: 'center',
-						justifyContent: 'space-between',
-					}}
-				>
-					<Typography
-						value='Amount due'
-						variant='body'
-						colour='lightgray'
-					/>
-					<Typography
-						value='0.00410775 BTC'
-						variant='body'
-						colour='gray'
-					/>
-				</div>
+				{payinCurrency && (
+					<>
+						<div
+							style={{
+								display: 'flex',
+								flexDirection: 'row',
+								alignItems: 'center',
+								justifyContent: 'space-between',
+							}}
+						>
+							<Typography
+								value='Amount due'
+								variant='body'
+								colour='lightgray'
+							/>
+							<Typography
+								value={`${quote?.paidCurrency.amount} ${quote?.paidCurrency.currency}`}
+								variant='body'
+								colour='gray'
+							/>
+						</div>
 
-				<div
-					style={{
-						display: 'flex',
-						flexDirection: 'row',
-						alignItems: 'center',
-						justifyContent: 'space-between',
-					}}
-				>
-					<Typography
-						value='Quote expires in'
-						variant='body'
-						colour='lightgray'
-					/>
-					<Typography
-						value='00:00:31'
-						variant='body'
-						colour='gray'
-					/>
-				</div>
+						<div
+							style={{
+								display: 'flex',
+								flexDirection: 'row',
+								alignItems: 'center',
+								justifyContent: 'space-between',
+							}}
+						>
+							<Typography
+								value='Quote expires in'
+								variant='body'
+								colour='lightgray'
+							/>
+							<Typography
+								value={formatTimeFromTimestamps(quote?.expiryDate!)}
+								variant='body'
+								colour='gray'
+							/>
+						</div>
+					</>
+				)}
 			</div>
 
 			<div>
-				<button style={{ display: 'inline-block', width: '100%' }}>
+				<button
+					style={{ display: 'inline-block', width: '100%' }}
+					onClick={handleConfirmation}
+				>
 					Confirm
 				</button>
 			</div>
