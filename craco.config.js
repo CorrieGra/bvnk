56765896fd3c335d3e9d88113226a185370f3be4
Corrie Graham <corriegdev@gmail.com>
2025-04-07 17:58:41 +0200

    chore(dto): Added dto and alias

--- craco.config.js
+++ craco.config.js
@@ -1,14 +1,15 @@
 const path = require('path');
 
 module.exports = {
-  webpack: {
-    alias: {
-      'app': path.resolve(__dirname, 'src/app'),
-      'assets': path.resolve(__dirname, 'src/assets'),
-      'components': path.resolve(__dirname, 'src/components'),
-      'features': path.resolve(__dirname, 'src/features'),
-      'pages': path.resolve(__dirname, 'src/pages'),
-      'services': path.resolve(__dirname, 'src/services'),
-    },
-  },
+	webpack: {
+		alias: {
+			app: path.resolve(__dirname, 'src/app'),
+			assets: path.resolve(__dirname, 'src/assets'),
+			components: path.resolve(__dirname, 'src/components'),
+			features: path.resolve(__dirname, 'src/features'),
+			pages: path.resolve(__dirname, 'src/pages'),
+			services: path.resolve(__dirname, 'src/services'),
+			dto: path.resolve(__dirname, 'src/dto'),
+		},
+	},
 };
