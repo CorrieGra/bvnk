c5ee317789309dd858b361531fffe9de74fa82be
Corrie Graham <corriegdev@gmail.com>
2025-04-07 10:48:25 +0200

    refactor(comment): Removed comment

--- src/index.tsx
+++ src/index.tsx
@@ -25,10 +25,6 @@ root.render(
 	</React.StrictMode>,
 );
 
-// "Accept Quote": <DOMAIN>/payin/<UUID>
-// "Pay Quote": <DOMAIN>/payin/<UUID>/pay
-// "Expiry": <DOMAIN>/payin/<UUID>/expired
-
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
