f8efcd6a34af2171922cf1970c5b06604243468a
Corrie Graham <corriegdev@gmail.com>
2025-04-08 18:12:46 +0200

    refactor(molecule): Extracted into QRCode component

--- src/components/molecule/index.ts
+++ src/components/molecule/index.ts
@@ -2,5 +2,6 @@ import { Card } from './card';
 import { Select } from './select';
 import { List } from './list';
 import { Button } from './button';
+import { QRCode } from './qr-code';
 
-export { Card, Select, List, Button };
+export { Card, Select, List, Button, QRCode };
